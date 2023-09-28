af9de27e1added7c724360b9c6ea97b20d810f0a
Poruton <98906235+Poruton@users.noreply.github.com>
2023-09-28 14:37:50 +0200

    Update check.py

--- syntax checker/check.py
+++ syntax checker/check.py
@@ -1,5 +1,3 @@
-#Beetje testen
-
 #  ____  __.          ___.                      
 # |    |/ _|   ____   \_ |__   _______  _____   
 # |      <    /  _ \   | __ \  \_  __ \ \__  \  
@@ -7,12 +5,18 @@
 # |____|__ \  \____/   |___  /  |__|    (____  /
 #         \/               \/                \/ 
 
+
+import Token
+
 # leest vanuit de command line in
 invoer = input('---< ')
 
 geknipt = invoer.split(' ')
 #filtert de lege elementen uit geknipt
-gefiltert = list(filter(None, geknipt))
+gefilterd = list(filter(None, geknipt))
 
 print(geknipt)
-print(gefiltert)
+print(gefilterd)
+
+Token.verwerk(gefilterd)
+print(gefilterd)
