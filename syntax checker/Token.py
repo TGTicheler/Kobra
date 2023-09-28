31d3ca9ffaefa9a5242bdae20cac13053fe9106a
Poruton <98906235+Poruton@users.noreply.github.com>
2023-09-28 14:37:34 +0200

    Update Token.py

--- syntax checker/Token.py
+++ syntax checker/Token.py
@@ -1,15 +1,9 @@
-LHAAK = 'LINKER HAAKJE'
-RHAAK = 'RECHTER HAAKJE'
-SLASH = 'SLASH'
+import string
+
+LHAAK = 'L-HAAKJE'
+RHAAK = 'R-HAAKJE'
+BSLASH = 'BSLASH'
 VAR = 'VARIABELE'
-
-
-
-
-
-
-
-
 
 class Token:
     def __init__(self, soort):
@@ -17,3 +11,26 @@ class Token:
 
     def __repr__(self):
         return f'{self.soort}'
+    
+
+def verwerk(invoer):
+    tokens = []
+    juist = True
+    for i in range(len(invoer)):
+        if invoer[i] == '(':
+            tokens.append(Token(LHAAK))
+        elif invoer[i] == ')':
+            tokens.append(Token(RHAAK))
+        elif invoer[i] == '\\':
+            tokens.append(Token(BSLASH))
+        elif invoer[i][0] in string.ascii_lowercase:
+            tokens.append(Token(VAR))
+        else:
+            print("Onjuiste invoer")
+            juist = False
+            
+    print(tokens)
+    return tokens, juist
+
+def hallo():
+    print('Hello world')
