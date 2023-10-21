0e93e7204788831a097819dd7033cbabaef3e0ea
Paul Tielens <paultielens03@gmail.com>
2023-10-21 22:40:16 +0200

    -

--- Opdracht 2/Kobra.py
+++ Opdracht 2/Kobra.py
@@ -15,8 +15,10 @@ import reductions
 # leest vanuit de command line in
 print("Filename: ")
 file = input('---< ')
+invoer = "\n"
 with open(file) as f:
-    invoer = f.readline()
+    while (invoer == "\n"): #pakt de eerste regel die tekst erin heeft staan
+        invoer = f.readline()
 verwerkt = Token.verwerk(invoer)
 geparset = Parser.Pars(verwerkt)
 root = geparset.parse()
@@ -24,5 +26,5 @@ reduceThis = reductions.reduce(root)
 reduced = reduceThis.run()
 
 
-
-exit(1)
+print("exit status 1")
+exit(0)
--- Opdracht 2/README.md
+++ Opdracht 2/README.md
@@ -1,7 +1,5 @@
 Paul Tielens s3612031
-
 Laura Faas s3443159
-
 Thom Ticheler s3696820
 
 # Kobra
