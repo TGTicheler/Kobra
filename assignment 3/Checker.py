c345e37c7889b4c2a972ac1af68fd41bf590ada8
Paul Tielens <paultielens03@gmail.com>
2023-11-02 12:11:38 +0100

    -

--- assignment 3/Checker.py
+++ assignment 3/Checker.py
@@ -1,3 +1,14 @@
+#  ____  __.          ___.                      
+# |    |/ _|   ____   \_ |__   _______  _____   
+# |      <    /  _ \   | __ \  \_  __ \ \__  \  
+# |    |  \  (  <_> )  | \_\ \  |  | \/  / __ \_
+# |____|__ \  \____/   |___  /  |__|    (____  /
+#         \/               \/                \/ 
+# Paul Tielens s3612031
+# Thom Ticheler s3696820
+# Laura Faas s3443159
+# Concepts of Programming Languages, 2023
+
 import Parser
 import Token
 
